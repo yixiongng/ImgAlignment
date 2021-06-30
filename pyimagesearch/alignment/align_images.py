eedadf7fe40c556b53804274fbb2738c918d1728
Yi Xiong Ng <yixiongng@gmail.com>
2021-07-01 02:43:16 +0800

    Updated param name

--- pyimagesearch/alignment/align_images.py
+++ pyimagesearch/alignment/align_images.py
@@ -2,11 +2,12 @@
 import numpy as np
 import imutils
 import cv2
+import glob
 
-def bf_SIFT(img1, img2):
+def bf_SIFT(image, template):
 	orb = cv2.SIFT_create()
-	kp1, des1 = orb.detectAndCompute(img1, None)
-	kp2, des2 = orb.detectAndCompute(img2, None)
+	kp1, des1 = orb.detectAndCompute(image, None)
+	kp2, des2 = orb.detectAndCompute(template, None)
 
 	# matcher takes normType, which is set to cv2.NORM_L2 for SIFT and SURF, cv2.NORM_HAMMING for ORB, FAST and BRIEF
 	bf = cv2.BFMatcher()
@@ -27,15 +28,15 @@ def bf_SIFT(img1, img2):
 			good.append([m])
 	
 	# draw matches
-	img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good[:100],None,flags=2)
+	img3 = cv2.drawMatchesKnn(image, kp1, template, kp2, good[:100],None,flags=2)
 	matchedVis = imutils.resize(img3, width=1000)
 	cv2.imshow("Matched Keypoints", matchedVis)
-	cv2.waitKey(0)
+	# cv2.waitKey(0)
 
 	# align image
 	src_points = np.float32([kp1[m.queryIdx].pt for m in goods]).reshape(-1, 1, 2)
 	dst_points = np.float32([kp2[m.trainIdx].pt for m in goods]).reshape(-1, 1, 2)
 	m, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 5.0)
-	aligned = cv2.warpPerspective(img1, m, (img2.shape[1], img2.shape[0]))
+	aligned = cv2.warpPerspective(image, m, (template.shape[1], template.shape[0]))
 	
 	return aligned
